43d05d3b46423671b2d8dbe11c160a5adba314e9
koraykoksal <kksl.koray@gmail.com>
2023-10-02 23:17:39 +0300

    update

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -8,15 +8,16 @@ const Todo = require('../models/todo')
 
 module.exports={
 
-    list:async(req,res)=>{
+    list: async (req, res) => {
 
+        // const data = await Todo.findAll()
         const data = await Todo.findAndCountAll()
-        //send metodunun önüne get status kodu eklenmiştir. default değeri 200
         res.status(200).send({
-            eror:false,
-            result:data
+            error: false,
+            result: data
         })
     },
+
     // CRUD METHODS:
 
     create: async (req, res) => {
